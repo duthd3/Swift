7e5d998ce3ad1bad6d50db9bc6b55d6194904b74
YeoSong <duthd3@naver.com>
2022-11-18 16:32:07 +0900

    Create for반복문.swift

---
+++ Swift기초/Day04/for반복문.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+// 0...5
+// 0,1,2,3,4,5
+
+//0..<5
+//0,1,2,3,4
+for index in 0...5{
+    print("호호 index: \(index)")
+}
+
+for index in 0..<5{
+    print("하하 index: \(index)")
+}
+
+for index in 0..<5 where index % 2 == 0 {
+    print("호호 짝수 index: \(index)")
+}
+
+var randomInts: [Int] = []
+
+for _ in 0..<25 {
+    let randomNumber = Int.random(in:0...100)
+    randomInts.append(randomNumber)
+}
+print("randomInts: \(randomInts)")
