6cd1ea9fb508fad5e7d9f15becaf79e98235f366
YeoSong <duthd3@naver.com>
2022-11-18 16:31:18 +0900

    Create enum케이스 나누기.swift

---
+++ Swift기초/Day03/enum케이스 나누기.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+//학교 - 초, 중, 고
+enum School{
+//    case elementary
+//    case middle
+//    case high
+    case elementary, middle, high
+}
+
+let yourSchool = School.high
+print("yourSchool: \(yourSchool)")
+
+enum Grade: Int{
+    case first = 1
+    case second = 2
+}
+
+let yourGrade = Grade.second
+print("yourGrade: \(yourGrade)")
+print("yourGrade: \(yourGrade.rawValue)")
+
+enum SchoolDetail{
+    case elementary(name: String)
+    case middle(name: String)
+    case high(name: String)
+    func getName() -> String{
+        switch self{
+        case .elementary(let name):
+            return name
+        case .middle(let name):
+            return name
+        case .high(let name):
+            return name
+        }
+    }
+}
+
+let yourMiddleSchoolName = SchoolDetail.middle(name: "서원중학교")
+print("yourMiddlSchoolName: \(yourMiddleSchoolName.getName())")
